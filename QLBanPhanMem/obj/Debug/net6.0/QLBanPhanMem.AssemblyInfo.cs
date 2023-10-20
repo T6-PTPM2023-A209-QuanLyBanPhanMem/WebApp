40343245cae973592e8a588aeed36ade3ca8af0b
android17x <qtrung1702@outlook.com>
2023-10-20 13:35:34 +0700

    commit

--- QLBanPhanMem/obj/Debug/net6.0/QLBanPhanMem.AssemblyInfo.cs
+++ QLBanPhanMem/obj/Debug/net6.0/QLBanPhanMem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBanPhanMem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0613fd197778722111032c0ffc37eadc450304e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBanPhanMem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBanPhanMem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
